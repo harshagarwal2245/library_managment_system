8d7b98452c699ef2c0d3e8e6062918d3366a5b81
Harsh Agarwal <agarwalharsh244@gmail.com>
2022-04-05 22:58:05 +0530

    Completed Api function to retrieve object

--- api/views.py
+++ api/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from shelf.models import Book
 from .serializers import BookSerializer
+from django.http import JsonResponse
 
 def BookApiView(request):
     """This function is created to retriev
     all books from database"""
     books = Book.objects.all()
     serializer = BookSerializer(books, many=True)
-    return render(request, 'book_api_list.html', {'books': serializer.data})
+    return JsonResponse(serializer.data, safe=False)
 
